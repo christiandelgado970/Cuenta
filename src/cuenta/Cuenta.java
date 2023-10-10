4181b88e2a3b87544671b7293e06b7ad9c175717
Cristian <Cristian@DESKTOP-1P2A66L>
2023-10-10 10:36:00 +0200

    TDD paso 1, creo las pruebas necesarias

---
+++ src/cuenta/Cuenta.java
@@ -0,0 +1,17 @@
+package cuenta;
+
+public class Cuenta {
+
+	public static void ingresar(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void retirar(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
